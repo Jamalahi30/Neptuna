85059fad70d44090cd6a2629b84324b41b6c5972
Amaanatu DIOP <jamalahi30@gmail.com>
2018-01-20 17:50:36 +0000

    Created login activity and register activity

---
+++ app/src/main/java/io/jamalahi/neptuna/RegisterActivity.java
@@ -0,0 +1,18 @@
+package io.jamalahi.neptuna;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amanatu on 20/01/18.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
